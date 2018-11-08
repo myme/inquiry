8c3313542d02bcd0ba45fefec0830c81d209f8f7
Martin Øinæs Myrseth <myrseth@gmail.com>
2018-11-09 00:51:12 +0100

    input: Add getInput

--- src/Inquiry/Input.hs
+++ src/Inquiry/Input.hs
@@ -2,6 +2,7 @@
 
 module Inquiry.Input
   ( input
+  , getInput
   , setInput
   ) where
 
@@ -12,6 +13,9 @@ import           Data.Text.Zipper (textZipper, gotoEOL)
 input :: Text -> Text -> E.Editor Text Text
 input name value = E.applyEdit gotoEOL editor
   where editor = E.editorText name (Just 1) value
+
+getInput :: E.Editor Text Text -> Text
+getInput = foldr (<>) mempty . E.getEditContents
 
 setInput :: Text -> E.Editor Text Text -> E.Editor Text Text
 setInput text = E.applyEdit $ gotoEOL . const (textZipper [text] (Just 1))
