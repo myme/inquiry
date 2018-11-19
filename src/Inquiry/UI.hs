e60830a41e01f1c5266bcac56ee5105171a26d20
Martin Øinæs Myrseth <myrseth@gmail.com>
2018-11-19 15:20:17 +0100

    Properly render recent requests

--- src/Inquiry/UI.hs
+++ src/Inquiry/UI.hs
@@ -12,8 +12,8 @@ import           Brick.Widgets.Core (padBottom, padTopBottom, padLeftRight, txt,
 import qualified Brick.Widgets.Edit as E
 import           Data.Foldable (toList)
 import           Data.Maybe (fromMaybe)
-import           Data.Text (Text)
-import           Inquiry.Request (resBody, Request(..), Response, Method(..), reqMethod, RequestHistory)
+import           Data.Text (unpack, Text)
+import           Inquiry.Request (reqUrl, resBody, Request(..), Response, Method(..), reqMethod, RequestHistory)
 import           Inquiry.Types (showRecents, mode, urlInput, requestHistory, AppState, EditMode(..))
 import           Inquiry.Zipper (focus, emptyZipper)
 import           Lens.Micro.Platform ((^.))
@@ -25,8 +25,9 @@ drawRecents recent = centerLayer $
     padLeftRight 10
     recents'
   where recents' | recent == emptyZipper = txt "No recent requests!"
-                 | otherwise = foldr ((<=>) . str . show) emptyWidget list
+                 | otherwise = foldr ((<=>) . str . reqRepr) emptyWidget list
         list = reverse $ toList recent
+        reqRepr (r, _) = show (r ^. reqMethod) <> " " <> unpack (r ^. reqUrl)
 
 drawResponse :: Maybe Response -> Widget a
 drawResponse response' = borderWithLabel (txt "Response") $ padRight Max $ padBottom Max text
